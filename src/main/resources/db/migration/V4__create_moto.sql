96a527dd9d1b413a3d1bae0212cc456d5704b416
Otavio Miklos <otaviomiklos@gmail.com>
2025-09-30 02:09:27 -0300

    chore: sqlserver migration

--- src/main/resources/db/migration/V4__create_moto.sql
+++ src/main/resources/db/migration/V4__create_moto.sql
@@ -1,13 +1,11 @@
-create table sprint3_moto (
-   id_moto    number(5)
-      generated always as identity,
-   id_filial  number(5) not null,
-   id_usuario number(5) not null,
-   ds_placa   varchar2(20) not null,
-   ds_status  varchar2(20) not null,
-   constraint pk_moto primary key ( id_moto ),
-   constraint fk_filial foreign key ( id_filial )
-      references sprint3_filial ( id_filial ),
-   constraint fk_usuario foreign key ( id_usuario )
-      references sprint3_usuario ( id_usuario )
+CREATE TABLE sprint3_moto (
+    id_moto INT IDENTITY(1,1) PRIMARY KEY,
+    id_filial INT NOT NULL,
+    id_usuario INT NOT NULL,
+    ds_placa NVARCHAR(20) NOT NULL,
+    ds_status NVARCHAR(20) NOT NULL,
+    CONSTRAINT fk_filial FOREIGN KEY (id_filial)
+        REFERENCES sprint3_filial (id_filial),
+    CONSTRAINT fk_usuario FOREIGN KEY (id_usuario)
+        REFERENCES sprint3_usuario (id_usuario)
 );
--- src/main/resources/db/migration/V5__inital_data.sql
+++ src/main/resources/db/migration/V5__inital_data.sql
@@ -1,11 +1,9 @@
-insert into sprint3_filial (
-   nm_filial,
-   ds_endereco
-) values ( 'Osasco',
-           'Rua dos Bobos, 123' );
+INSERT INTO sprint3_filial (
+    nm_filial,
+    ds_endereco
+) VALUES ('Osasco', 'Rua dos Bobos, 123');
 
-insert into sprint3_filial (
-   nm_filial,
-   ds_endereco
-) values ( 'Barueri',
-           'Rua dos Mais Bobos, 456' );
+INSERT INTO sprint3_filial (
+    nm_filial,
+    ds_endereco
+) VALUES ('Barueri', 'Rua dos Mais Bobos, 456');
